094f5ebf1db7cd221d73f314112ee8197c746a4c
Hendrys Tobar <fabiantobar@gmail.com>
2019-07-15 18:35:05 -0500

    Agregada stack para Collections  y Boostrap con Bootstrap UIO

---
+++ tests/Fixture/CollectionsFixture.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * CollectionsFixture
+ */
+class CollectionsFixture extends TestFixture
+{
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'cod' => ['type' => 'string', 'length' => 3, 'null' => false, 'default' => null, 'collate' => 'utf8_bin', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'name' => ['type' => 'string', 'length' => 50, 'null' => false, 'default' => null, 'collate' => 'utf8_bin', 'comment' => '', 'precision' => null, 'fixed' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['cod'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8_bin'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init()
+    {
+        $this->records = [
+            [
+                'cod' => '0fefe85b-9553-4e32-a687-f0355bb21580',
+                'name' => 'Lorem ipsum dolor sit amet'
+            ],
+        ];
+        parent::init();
+    }
+}
