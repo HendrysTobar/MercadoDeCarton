9ac9dda053b7813049482bd6aebf24bf2591fc76
Hendrys Tobar <fabiantobar@gmail.com>
2020-02-18 19:50:36 -0500

    Creado archivo para arrancar Apache y  MySQl como tarea desde VS code

---
+++ UtilScripts/StartApacheMySQL.sh
@@ -0,0 +1,8 @@
+
+#Ejecuta Apache y MySQL en este equipo (con Windows)
+#Esto utiliza sudo, en windows debe tener instalado Win-sudo
+#Para instalar seguir las instrucciones de:
+#https://github.com/imachug/win-sudo
+
+sudo net start Apache2.4
+sudo net start mysql
